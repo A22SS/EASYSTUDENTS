2567b77f559583ebaaa9a800bca8600b5c8ebb91
A22SS <165214918+A22SS@users.noreply.github.com>
2026-04-16 11:28:13 +0200

    Create ScannerDisplay component

---
+++ frontend/src/components/ScannerDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScannerDisplay = () => {
+    return (
+        <div>
+            <h1>Scanner Display</h1>
+            <p>This is the scanner display component for the EasyStudents application.</p>
+        </div>
+    );
+};
+
+export default ScannerDisplay;
